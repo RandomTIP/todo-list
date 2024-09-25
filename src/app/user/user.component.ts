eecd468ff1a63d426d493a577803e2a4765d3e9a
Ramaz <bluashviliramaz3@gmail.com>
2024-09-25 17:21:34 +0400

    Refactor header and user components

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+let isUserNameUpperCase = false;
 
 @Component({
   selector: 'app-user',
@@ -8,5 +12,20 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get selectedUserImagePath() {
+    return `../../assets/users/${this.selectedUser.avatar}`;
+  }
+
+  onUserSelected() {
+    if(!isUserNameUpperCase){
+      this.selectedUser.name = this.selectedUser.name.toUpperCase();
+    }
+    else{
+      this.selectedUser.name = this.selectedUser.name.toLowerCase();
+    }
+
+    isUserNameUpperCase = !isUserNameUpperCase;
+  }
 }
